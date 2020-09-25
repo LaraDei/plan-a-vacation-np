8c7114b8a8c17125751a0e2b80246c269f72fcec
Lara Samuelson <lara.samuelson@gmail.com>
2020-09-25 08:39:26 -0700

    fixed formatquery bug and added notification if zero results

--- index.js
+++ index.js
@@ -6,28 +6,33 @@ const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${params[key].replace(/ /g, '')}`)
   return queryItems.join('&');
 }
 
 function displayResults(responseJson) {
   // if there are previous results, remove them
   console.log(responseJson);
+  
   $('#results-list').empty();
+  if (responseJson.data.length === 0){
+    $('#results-list').append(
+        '<h3>Zero search results</h3>'
+    )
+    
+   }
   // iterate through the items array
-  for (let i = 0; i < responseJson.data.length; i++){
-    // for each video object in the items 
-    //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
+  for (let i = 0; i < responseJson.data.length; i++){    
+    // for each object in the data array, add a list item to the results 
+    //list with the title, address, description, and url
     $('#results-list').append(
       `<li><h3>${responseJson.data[i].fullName}</h3>
       <p>${responseJson.data[i].addresses[0].line1}, ${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode} ${responseJson.data[i].addresses[0].postalCode} </p>
       <p>${responseJson.data[i].description}</p>
       <a href='${responseJson.data[i].url}'>${responseJson.data[i].url}</a>
       </li>`
-    )};
+    )
+    };
   //display the results section  
   $('#results').removeClass('hidden');
 };
